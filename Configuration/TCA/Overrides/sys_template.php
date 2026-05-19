60b7362b1228908a61d15448835c142bffbc8397
Nitin Parri <nitin@refresj.nl>
2026-05-19 16:53:54 +0200

    Update sys_template.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,14 +1,13 @@
 <?php
-defined('TYPO3_MODE') || die('Access denied.');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+declare(strict_types=1);
+
+defined('TYPO3') || die('Access denied.');
+
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+
+ExtensionManagementUtility::addStaticFile(
     'yoast_news',
     'Configuration/TypoScript',
     'Yoast SEO for TYPO3 - EXT:news'
-);
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    'yoast_news',
-    'Configuration/TypoScript/CMS8',
-    'Yoast SEO for TYPO3 - EXT:news (CMS8)'
 );
