707bace99c3a47d4cc72b10369e0133f2e44f62a
Nitin Parri <nitin@refresj.nl>
2026-05-19 16:57:26 +0200

    Update ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -5,11 +5,13 @@ CREATE TABLE tx_news_domain_model_news (
   tx_yoastseo_focuskeyword varchar(100) DEFAULT '' NOT NULL,
   og_title varchar(255) DEFAULT '' NOT NULL,
   og_description text,
-  og_image int(11) unsigned DEFAULT '0' NOT NULL,
+  og_image int unsigned DEFAULT 0 NOT NULL,
   twitter_title varchar(255) DEFAULT '' NOT NULL,
   twitter_description text,
-  twitter_image int(11) unsigned DEFAULT '0' NOT NULL,
-  tx_yoastseo_snippetpreview tinyint(3) DEFAULT '0' NOT NULL,
-  tx_yoastseo_readability_analysis tinyint(3) DEFAULT '0' NOT NULL,
-  tx_yoastseo_focuskeyword_analysis tinyint(3) DEFAULT '0' NOT NULL
+  twitter_image int unsigned DEFAULT 0 NOT NULL,
+  tx_yoastseo_snippetpreview tinyint unsigned DEFAULT 0 NOT NULL,
+  tx_yoastseo_readability_analysis tinyint unsigned DEFAULT 0 NOT NULL,
+  tx_yoastseo_focuskeyword_analysis tinyint unsigned DEFAULT 0 NOT NULL,
+  tx_yoastseo_score_readability varchar(50) DEFAULT '' NOT NULL,
+  tx_yoastseo_score_seo varchar(50) DEFAULT '' NOT NULL
 );
