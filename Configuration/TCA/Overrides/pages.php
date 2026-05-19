9dd8b3bf1f6eea6ec12df9cb9fee60d60745b843
Nitin Parri <nitin@refresj.nl>
2026-05-19 16:53:28 +0200

    Delete Configuration/TCA/Overrides/pages.php

--- Configuration/TCA/Overrides/pages.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined('TYPO3_MODE') || die('Access denied.');
-
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-    'yoast_news',
-    'Configuration/TSconfig/Page.tsconfig',
-    'Yoast SEO for TYPO3 - EXT:news'
-);
